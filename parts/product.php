749713eb845483509df7ae5db4b37611bc48d6f8
kevinchap <kevin.chapron@hetic.net>
2015-12-22 22:02:46 +0100

    [Product] add one point for inte

--- parts/product.php
+++ parts/product.php
@@ -1,3 +1,23 @@
 <section id="product" class="section  slide-section">
 	<h3 class="vertical-align background-text">displ’ever</h3>
+	<section id="content-product">
+		<section class="section">
+			<div class="point">
+				<span class="number" style="left:-400px;top:-210px;">01</span>
+				<img class="big" style="left:-505px;top:-150px;" src="./images/product-01-big.jpg">
+				<img class="small" style="left:-10px;top:100px;" src="./images/product-01-small.jpg">
+				<div class="text-content border-left" style="left:240px;top:-140px;width:330px;">
+					<h2>Universal wireless<br/>batteryless room<br/>controller</h2>
+					<ul>
+						<li>Ambient settings</li>
+						<li>Smart Temperature control</li>
+						<li><strong>Energy consumption</strong> display</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<section class="section"></section>
+		<section class="section"></section>
+		<section class="section"></section>
+	</section>
 </section>
